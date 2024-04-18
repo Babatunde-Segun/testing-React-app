79d52ce7238a93f55f6c29081eb64cd9e618210a
Babatunde-Segun <officiallyvincify@gmail.com>
2024-04-18 09:35:34 +0100

    test suite and userEvent

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+import { useState } from "react";
 
 // Component to be tested
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>Happy to see you</p>
+      {!changedText && <p>Happy to see you</p>}
+      {changedText && <p>Changed!</p>}
+
+      <button onClick={changeTextHandler}>Changed Text</button>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,20 +1,66 @@
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
-// Basically we want to do three things when we write a test by using three's A
-// 1) Arrange: Set up the test data, test environment
-//  2) Act: Run logic that should be tested( eg execute function)
-// 3) Assert: Compare execution result with expectated result
+// describe function are used to create test suites are group of tests related to a component or function so as to make things easier
+describe("Greeting component", () => {
+  // Basically we want to do three things when we write a test by using three's A
+  // 1) Arrange: Set up the test data, test environment
+  //  2) Act: Run logic that should be tested( eg execute function)
+  // 3) Assert: Compare execution result with expectated result
 
-// test is a global function which take two arguments, first the description and a anonymous function to be executed
-test("renders Hello World as a text", () => {
-  // Arrange
-  render(<Greeting />);
+  // test is a global function which take two arguments, first the description and a anonymous function to be executed
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // basically nothing for now
+    // Act
+    // basically nothing for now
 
-  // Assert
-  const helloWorld = screen.getByText("Hello World", { exact: false });
-  expect(helloWorld).toBeInTheDocument();
+    // Assert
+    const helloWorld = screen.getByText("Hello World", { exact: false });
+    expect(helloWorld).toBeInTheDocument();
+  });
+
+  test("render Happy to see you as a text was not clicked", () => {
+    // arrange
+    render(<Greeting />);
+
+    // act
+    // nothing
+
+    // Assert
+    const happyToSeeText = screen.getByText("Happy to see you", {
+      exact: false,
+    });
+    expect(happyToSeeText).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    // arrange
+    render(<Greeting />);
+
+    // act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders happy to see you  when the button was clicked", () => {
+    // act
+    render(<Greeting />);
+
+    // act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // assert
+    const happyToSeeText = screen.queryByText("happy to see you", {
+      exact: false,
+    });
+    expect(happyToSeeText).toBeNull();
+  });
 });
