24c41fdc1fe383f13caf3ef86eccd26628c35818
Babatunde-Segun <officiallyvincify@gmail.com>
2024-04-23 23:09:25 +0100

    mock test

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,12 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders post if request succeeds", async () => {
+    // mock function
+    window.fetch = jest.fn();
+
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "first post" }],
+    });
     // Arrange
     render(<Async />);
 
